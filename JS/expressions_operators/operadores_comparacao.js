a3248e4fabc221e683cf628a265b6641c18e62da
Rpaduan <renato_paduan@hotmail.com>
2026-05-11 10:00:48 -0300

    estudos js

---
+++ JS/expressions_operators/operadores_comparacao.js
@@ -0,0 +1,15 @@
+// Operadores de comparação
+
+// Irá comparar valores e retornar um Boolean como resposta à comparação
+
+let one = 1
+let two = 2
+
+// == igual a 
+console.log(two == 1)
+console.log(one == "1") // o == só avalia o valor, 1, e não o tipo, "1", para diferenciar o tipo usamos === (estritamente igual).
+
+// != diferente de 
+console.log(one != two)
+console.log(one != 1)
+console.log(one != "1") // o != só avalia o valor, 1, e não o tipo, "1", para diferenciar o tipo usamos !== (estritamente diferente)
