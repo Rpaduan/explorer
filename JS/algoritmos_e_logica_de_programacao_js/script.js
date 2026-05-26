327f0a43636e4769b99a8f1cd3bfb80ac807d6d8
Rpaduan <renato_paduan@hotmail.com>
2026-05-26 12:14:02 -0300

    estudos js

--- JS/algoritmos_e_logica_de_programacao_js/script.js
+++ JS/algoritmos_e_logica_de_programacao_js/script.js
@@ -8,3 +8,46 @@
     Crie uma função para fazer o cálculo de IMC
 */
 
+/* peso / (altura * altura) */
+
+const patients = [
+    {
+        name: "Renato",
+        sex: "male",
+        age: 38,
+        weight: 74,
+        height: 168,
+    },
+    {
+        name: "Jéssica",
+        sex: "female",
+        age: 38,
+        weight: 100,
+        height: 170,
+    },
+    {
+        name: "Alice",
+        sex: "female",
+        age: 10,
+        weight: 35,
+        height: 135,
+    },
+]
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
+}
+
+function printPatientIMC(patient) {
+
+    let article = patient.sex == "male"
+    ? "O paciente"
+    : "A paciente"    
+
+    return `${article} ${patient.name} possui o IMC de : ${IMC(patient.weight, patient.height)}`
+}
+
+for (let patient of patients) {
+    let IMCmessege = printPatientIMC(patient)
+    alert(IMCmessege)
+}
